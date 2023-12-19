51dfb311985665d621732e227a9ba7a7aa00a157
Woochang1132 <show4031132@gmail.com>
2023-12-19 17:37:00 +0900

    react-repeat => component 만들기

---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Profile() {
+    return (
+          <div className='nature'>
+            <img className='nature__img' 
+            src="https://images.unsplash.com/photo-1701906268448-6c559049e58a?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+            alt="nature" 
+            />
+            <h1>사막을 배경으로 한 </h1>
+            <p>사막을 걷는다면</p>
+          </div>
+    );
+}
+
