30a33a9ffbf197c0f5c61df58ab2b364c5f5be99
Woochang1132 <show4031132@gmail.com>
2023-12-20 10:41:12 +0900

    react-repeat => 조건에 따른 태크 추가하기

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -4,6 +4,11 @@ import "./App.css";
 
 export default function AppProfile() {
   return <div>
-    <Profile/>
+    <Profile 
+    url='https://images.unsplash.com/photo-1702517207949-a02e0cbc695d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    name="James Kim"
+    title="경기 시작 전"
+    isTure = "true"
+    />
   </div>;
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
-export default function Profile() {
+export default function Profile({url, name, title, isTrue}) {
     return (
-          <div className='nature'>
-            <img className='nature__img' 
-            src="https://images.unsplash.com/photo-1701906268448-6c559049e58a?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-            alt="nature" 
+          <div className='profile'>
+            <img className='profile__img' 
+            src={url} 
+            alt="profile" 
             />
-            <h1>사막을 배경으로 한 </h1>
-            <p>사막을 걷는다면</p>
+            {/* {isTrue ? null : <p className='new'>new</p>} */}
+            {isTrue && <p className='new'>new</p>}
+            <h1>{name}</h1>
+            <p>{title}</p>
           </div>
     );
 }
