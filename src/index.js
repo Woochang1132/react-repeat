8c7d6e3301803f9b9387476381c07112cd722a67
Woochang1132 <show4031132@gmail.com>
2023-12-22 15:01:22 +0900

    react-repeat => Immer 속성 및 활용하기

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import "./index.css";
 import App from "./App.js";
 import AppForm from "./AppForm.jsx";
 import AppMentors from "./AppMentors.jsx";
+import AppMentorsImmer from "./AppMentorsImmer.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppMentors />
+    <AppMentorsImmer />
   </React.StrictMode>
 );
