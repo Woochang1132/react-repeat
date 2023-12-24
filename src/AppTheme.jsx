77db2bc4e0ecb870343d068e80734fc7f92a0e91
Woochang1132 <show4031132@gmail.com>
2023-12-24 12:48:47 +0900

    react-repeat => ContextProvider를 통한 value 값 활용

--- src/AppTheme.jsx
+++ src/AppTheme.jsx
@@ -1,14 +1,70 @@
-import React from 'react';
+import { useContext } from 'react';
+import { DarkMdoeContext, DarkModeProvider } from './context/DarkModeContext';
 
 export default function AppTheme() {
     return (
         <div>
+            <DarkModeProvider>
             <Header/>
+            <Main/>
+            </DarkModeProvider>
         </div>
     );
 }
 
 function Header(){
     return <header className='header'>Header</header>
+}
+
+function Main(){
+    return(
+        <main className='main'>
+            Main
+            <Profile/>
+            <Products/>
+        </main>
+    )
+}
+
+function Profile(){
+    return(
+        <div>
+            Profile
+            <User/>
+        </div>
+    )
+}
+
+function User(){
+    return <div>
+        User
+    </div>
+}
+
+function Products(){
+    return(
+        <div>
+            Products
+            <ProductDetail/>
+        </div>
+    )
+}
+
+function ProductDetail(){
+    const {darkMode, toggleDarkMode} = useContext(DarkMdoeContext); 
+    return (
+        <div>
+            ProductDetail
+            <p>
+                DarkMode:
+                {
+                    darkMode ? (<span style={{backgroundColor: 'black', color: 'white'}}>Dark Mode</span>)
+
+                    : (<span>Light mode</span>)
+                }
+            </p>
+            <button onClick={() => toggleDarkMode()}>Toggle</button>
+        </div>
+    )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.js";
-import AppForm from "./AppForm.jsx";
-import AppMentors from "./AppMentors.jsx";
-import AppMentorsImmer from "./AppMentorsImmer.jsx";
-import Products from "./components/Products.jsx";
-import AddProducts from "./AddProducts.jsx";
+import AppTheme from "./AppTheme.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AddProducts />
+    <AppTheme />
   </React.StrictMode>
 );
